557e2dcd193bfa319eec225f122042506dbc33a2
Alfredo Mendoza <almendoza25@gmail.com>
2018-11-07 10:30:07 -0800

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    require: true,
+    required: true,
     trim: true,
     minlength: 1,
     unique: true,
@@ -44,13 +44,30 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens = user.tokens.concat([{access, token}]);
+  user.tokens.push({access, token});
 
   return user.save().then(() => {
     return token;
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = {User}
